11be1fb357b3ef76901a3e7e8ca1ebde97f59440
Matt Raykowski <matt.raykowski@gmail.com>
2013-04-16 11:53:22 -0500

    Finished static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,10 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+# Capybara Integration
+require 'capybara/rspec'
+require 'capybara/rails'
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
@@ -35,4 +39,6 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  config.include Capybara::DSL
 end
